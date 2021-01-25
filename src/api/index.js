1a3f47b8f7ae56fdb8240efc07e2b9c9801d399d
Emanuel Cardozo <leuname.01@gmail.com>
2021-01-25 11:14:14 -0300

    Adding the lost api method

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,15 @@ const API_URL = "https://pokeapi.co/api/v2/pokemon";
 const ITEMS_PER_PAGE = 5;
 
 const API = {
-  getPokemons: (page) => {
+  getPokemons: page => {
     const req = new Request(`${ API_URL }?offset=${(page-1)* ITEMS_PER_PAGE }&limit=${ITEMS_PER_PAGE}`);
+    return fetch(req)
+      .then(res => {
+        return res.json()
+      })
+  },
+  getPokemon: id => {
+    const req = new Request(`${ API_URL }/${id}`);
     return fetch(req)
       .then(res => {
         return res.json()
