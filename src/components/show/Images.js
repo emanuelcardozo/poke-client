ad3d7cd3a3c86796ac277f7f3c3e19dd889bef4d
Emanuel Cardozo <leuname.01@gmail.com>
2021-01-25 11:43:26 -0300

    Showing a SVG as a main image

--- src/components/show/Images.js
+++ src/components/show/Images.js
@@ -3,9 +3,12 @@ import utils from "../../lib/utils"
 
 function Images(props){
   const { sprites } = props
+  const { front_default } = sprites.other.dream_world
 
   return(
     <React.Fragment>
+    { front_default &&
+      <img src={front_default} alt={"front_default"} /> }
     {
       Object.keys(sprites).map((key, i) => {
         const url = sprites[key];
