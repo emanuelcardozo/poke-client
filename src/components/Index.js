d17af59897300f2ffcbb6888cd96bf60d7bfe136
Emanuel Cardozo <leuname.01@gmail.com>
2021-01-27 11:36:47 -0300

    Moving loading from Index to List component

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,6 +1,5 @@
 import API from "../api/index.js"
 import { useState, useEffect } from "react"
-import Loading from "./utils/Loading"
 import Paginator from "./utils/Paginator"
 import List from "./index/List"
 
@@ -8,7 +7,7 @@ function Index({ location, history }) {
   const { search } = location;
   const pageString = search.split("page=")[1];
   const page = parseInt(pageString || 1);
-  const [ state, setState ] = useState({ pokemons: [], count: 0, loading: true});
+  const [ state, setState ] = useState({ pokemons: [], count: 0 });
 
   useEffect(() => {
     API.getPokemons(page)
@@ -16,30 +15,23 @@ function Index({ location, history }) {
         if( response.results.length === 0 )
           throw { type: "error", description: "Wrong Page"}
 
-        setState({ pokemons: response.results, count: response.count, loading: false })
+        setState({ pokemons: response.results, count: response.count })
       }).catch( e => {
         history.push("/?page=1")
       })
   }, [page])
 
   const handleClick = (page)=>{
-    setState({ ...state, loading: true })
+    setState({ ...state, pokemons: [] })
     history.push("?page=" + page)
   }
-
-  let Component = null;
-
-  if(state.loading)
-    Component = Loading;
-  else
-    Component = List;
 
   return(
     <div className="container">
       <div className="row">
         <div className="col-md-8 margin_auto">
           <div className="list-container">
-            <Component pokemons={ state.pokemons } />
+            <List pokemons={ state.pokemons } />
           </div>
           <Paginator handleClick={ handleClick } items={ state.count } currentPage={ page } itemsPerPage={ 5 } />
         </div>
--- src/components/index/List.js
+++ src/components/index/List.js
@@ -1,7 +1,12 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import Loading from "../utils/Loading"
 
 function List({ pokemons }) {
+
+  if(!pokemons.length)
+    return <Loading />
+
   return(
     <div className="list">
     { pokemons.map((pokemon, i)=>{
