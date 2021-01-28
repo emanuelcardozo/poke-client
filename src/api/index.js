afab5fb03536de3467ca61e257350553e0114e10
Emanuel Cardozo <leuname.01@gmail.com>
2021-01-28 13:38:48 -0300

    Adding style to info panel

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,29 @@
-const API_URL = "https://pokeapi.co/api/v2/pokemon";
+const API_URL = "https://pokeapi.co/api/v2";
 const ITEMS_PER_PAGE = 5;
 
 const API = {
   getPokemons: page => {
-    const req = new Request(`${ API_URL }?offset=${(page-1)* ITEMS_PER_PAGE }&limit=${ITEMS_PER_PAGE}`);
+    const req = new Request(`${ API_URL }/pokemon?offset=${(page-1)* ITEMS_PER_PAGE }&limit=${ITEMS_PER_PAGE}`);
     return fetch(req)
       .then(res => {
         return res.json()
       })
   },
-  getPokemon: id => {
-    const req = new Request(`${ API_URL }/${id}`);
-    return fetch(req)
+  getPokemon: async id => {
+    const reqPokemon = new Request(`${ API_URL }/pokemon/${id}`);
+    const reqCharacteristic = new Request(`${ API_URL }/characteristic/${id}`);
+
+    const pokemon = await fetch(reqPokemon)
       .then(res => {
         return res.json()
       })
+
+    const characteristic = await fetch(reqCharacteristic)
+      .then(res => {
+        return res.json()
+      })
+
+    return { ...pokemon, characteristic }
   }
 }
 
--- src/components/show/Info.js
+++ src/components/show/Info.js
@@ -3,10 +3,22 @@ import Container from "../utils/Container"
 function Info({ pokemon }) {
   return(
     <Container className="info_container">
-      <h3>Info:</h3>
-      <p>{ `Height: ${pokemon.height}` }</p>
-      <p>{ `Weight: ${pokemon.weight}` }</p>
-      <p>{ `Base XP: ${pokemon.base_experience}` }</p>
+      <h3>General</h3>
+      <div className="info_body">
+        <div className="row">
+          <div className="col-md-6">
+            <label>Height</label><p>{pokemon.height}</p>
+            <label>Base XP</label><p>{pokemon.base_experience}</p>
+          </div>
+          <div className="col-md-6">
+            <label>Weight</label><p>{pokemon.weight}</p>
+            <label>Highest Stat</label><p>{pokemon.characteristic.highest_stat.name}</p>
+          </div>
+        </div>
+        <div className="info_description">
+          <label>Description</label><p>{pokemon.characteristic.descriptions[2].description}</p>
+        </div>
+      </div>
     </Container>
   )
 }
