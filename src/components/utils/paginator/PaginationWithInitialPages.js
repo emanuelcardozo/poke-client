f85b181fc64974da974d3ffedcc87de67b396281
Emanuel Cardozo <leuname.01@gmail.com>
2021-01-29 12:59:21 -0300

    Changing == to ===

--- src/components/utils/paginator/PaginationWithInitialPages.js
+++ src/components/utils/paginator/PaginationWithInitialPages.js
@@ -7,15 +7,15 @@ function PaginationWithInitialPages({ currentPage, pages, handleClick, deviceWid
 
   return(
     <React.Fragment>
-      <Pagination.Item onClick={ (ev) => handleClick( 2 ) } active={ currentPage == 2 } >{ 2 }</Pagination.Item>
-      <Pagination.Item onClick={ (ev) => handleClick( 3 ) } active={ currentPage == 3 } >{ 3 }</Pagination.Item>
-      { showButton && <Pagination.Item onClick={ (ev) => handleClick( 4 ) } active={ currentPage == 4 } >{ 4 }</Pagination.Item>}
-      { showButton && <Pagination.Item onClick={ (ev) => handleClick( 5 ) } active={ currentPage == 5 } >{ 5 }</Pagination.Item>}
+      <Pagination.Item onClick={ (ev) => handleClick( 2 ) } active={ currentPage === 2 } >{ 2 }</Pagination.Item>
+      <Pagination.Item onClick={ (ev) => handleClick( 3 ) } active={ currentPage === 3 } >{ 3 }</Pagination.Item>
+      { showButton && <Pagination.Item onClick={ (ev) => handleClick( 4 ) } active={ currentPage === 4 } >{ 4 }</Pagination.Item>}
+      { showButton && <Pagination.Item onClick={ (ev) => handleClick( 5 ) } active={ currentPage === 5 } >{ 5 }</Pagination.Item>}
 
       <Pagination.Ellipsis className={ "ellipsis" } />
 
-      { showLessButton && <Pagination.Item onClick={ (ev) => handleClick( pages - 2 ) } active={ currentPage == pages - 2 } >{ pages - 2 }</Pagination.Item>}
-      <Pagination.Item onClick={ (ev) => handleClick( pages - 1 ) } active={ currentPage == pages - 1 } >{ pages - 1 }</Pagination.Item>
+      { showLessButton && <Pagination.Item onClick={ (ev) => handleClick( pages - 2 ) } active={ currentPage === pages - 2 } >{ pages - 2 }</Pagination.Item>}
+      <Pagination.Item onClick={ (ev) => handleClick( pages - 1 ) } active={ currentPage === pages - 1 } >{ pages - 1 }</Pagination.Item>
     </React.Fragment>
   )
 }
